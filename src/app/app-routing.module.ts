519988e2b8a2b8b0af4ae0e243d70fb0b43757af
Bram Jager <bramjager@upcmail.nl>
2021-09-16 12:06:36 +0200

    Changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { OnePageComponent } from './one-page/one-page.component';
 import { PhotoListComponent } from './photo-list/photo-list.component';
 import { PhotoComponent } from './photo/photo.component';
 
 const routes: Routes = [
   {path: "", component: PhotoListComponent},
-  {path: "photo/:id", component: PhotoComponent}
+  {path: "photo/:id", component: PhotoComponent},
+  {path: "onepage", component: OnePageComponent}
 ];
 
 @NgModule({
